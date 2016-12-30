ff91add7619cfad682b2a4f60966ef01d268cbb5
abkgit <elite.abhishek@gmail.com>
2016-12-31 03:33:00 +0530

    1.2 HabitContract coded to define database columns.

--- app/src/main/java/com/example/android/habittracker/HabitActivity.java
+++ app/src/main/java/com/example/android/habittracker/HabitActivity.java
@@ -1,13 +1,37 @@
 package com.example.android.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
+
+import com.example.android.habittracker.data.HabitContract.HabitEntry;
 
 public class HabitActivity extends AppCompatActivity {
+
+    /**
+     * Variable to save the current date in milliseconds
+     */
+    private long mDateToday;
+
+    /**
+     * Spinner field to select the habit name
+     */
+    private Spinner mGenderSpinner;
+
+    /**
+     * Name of the habit. The possible valid values are in the HabitContract.java file:
+     * 0 for "None tracked today", 1 for "Got to bed early", 2 for "Performed daily exercise",
+     * 3 for "Ate healthy food".
+     * {@link HabitEntry#HABIT_NONE}, {@link HabitEntry#HABIT_SLEEP},
+     * {@link HabitEntry#HABIT_EXERCISE}, or {@link HabitEntry#HABIT_EAT_HEALTHY}.
+     */
+    private int mHabit = HabitEntry.HABIT_NONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit);
     }
+
+
 }
--- app/src/main/java/com/example/android/habittracker/data/HabitContract.java
+++ app/src/main/java/com/example/android/habittracker/data/HabitContract.java
@@ -1,5 +1,55 @@
 package com.example.android.habittracker.data;
 
+import android.provider.BaseColumns;
 
+/**
+ * API Contract for the Habit Tracker app.
+ */
 public class HabitContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private HabitContract() {}
+
+    /**
+     * Inner class that defines constant values for the habits database table.
+     * Each entry in the table represents a single habit entry for the current date.
+     */
+    public static final class HabitEntry implements BaseColumns {
+
+        /** Name of database table for habits */
+        public final static String TABLE_NAME = "habits";
+
+        /**
+         * Unique ID number for the habit (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the habit.
+         *
+         * The only possible values are {@link #HABIT_NONE}, {@link #HABIT_SLEEP},
+         * {@link #HABIT_EXERCISE}, or {@link #HABIT_EAT_HEALTHY}.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_HABIT = "habit";
+
+        /**
+         * The date(in milliseconds) on which the habit was tracked.
+         *
+         * Type: long
+         */
+        public final static String COLUMN_HABIT_DATE = "date";
+
+        /**
+         * Possible values for the habits of the user.
+         */
+        public static final int HABIT_NONE = 0;
+        public static final int HABIT_SLEEP = 1;
+        public static final int HABIT_EXERCISE = 2;
+        public static final int HABIT_EAT_HEALTHY = 3;
+    }
 }
